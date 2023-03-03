22e995dff82644dc9579f47f6ed1b0da32166dde
belugafm <13643825+belugafm@users.noreply.github.com>
2023-03-03 20:05:58 +0900

    add support for chatgpt api

--- examples/longer_context.ts
+++ examples/longer_context.ts
@@ -3,6 +3,9 @@ import qs from "querystring"
 import { Configuration, OpenAIApi } from "openai"
 import { WebSocketClient } from "../websocket"
 import { ChannelObjectT, MessageObjectT } from "../object"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const consumerKey = process.env.CONSUMER_KEY || ""
 const consumerSecret = process.env.CONSUMER_SECRET || ""
@@ -22,290 +25,307 @@ console.log(accessToken)
 console.log(accessTokenSecret)
 
 const configuration = new Configuration({
-    organization: process.env.OPENAI_ORGANIZATION,
-    apiKey: process.env.OPENAI_API_KEY,
+  organization: process.env.OPENAI_ORGANIZATION,
+  apiKey: process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(configuration)
 
 const oauth = new OAuth.OAuth(
-    "https://beluga.fm/api/oauth/request_token",
-    "https://beluga.fm/api/oauth/access_token",
-    consumerKey,
-    consumerSecret,
-    "1.0",
-    null,
-    "HMAC-SHA1"
+  "https://beluga.fm/api/oauth/request_token",
+  "https://beluga.fm/api/oauth/access_token",
+  consumerKey,
+  consumerSecret,
+  "1.0",
+  null,
+  "HMAC-SHA1"
 )
 
 function getContextMessages(messages: MessageObjectT[]): MessageObjectT[] {
-    const maxTextLength = 250
-    const maxMessageCount = 7 // 最大何個の投稿を含めるか
-    const untilSeconds = 300 // 最大何秒前の投稿まで含めるか
-    const ret = []
-    let sumTextLength = 0
-    let latestCreatedAt = 0
-    for (const message of messages) {
-        if (message.text == null) {
-            continue
-        }
-        if (message.text.length > maxTextLength) {
-            continue
-        }
-        if (ret.length == 0) {
-            latestCreatedAt = new Date(message.created_at).getTime()
-        } else {
-            if (latestCreatedAt - new Date(message.created_at).getTime() > untilSeconds * 1000) {
-                break
-            }
-        }
-        ret.push(message)
-        sumTextLength += message.text.length
-        if (sumTextLength >= maxTextLength) {
-            break
-        }
-        if (ret.length >= maxMessageCount) {
-            break
-        }
+  const maxTextLength = 500
+  const maxMessageCount = 11 // 最大何個の投稿を含めるか
+  const untilSeconds = 300 // 最大何秒前の投稿まで含めるか
+  const ret = []
+  let sumTextLength = 0
+  let latestCreatedAt = 0
+  for (const message of messages) {
+    if (message.text == null) {
+      continue
     }
-    return ret
+    if (message.text.length > maxTextLength) {
+      continue
+    }
+    if (ret.length == 0) {
+      latestCreatedAt = new Date(message.created_at).getTime()
+    } else {
+      if (latestCreatedAt - new Date(message.created_at).getTime() > untilSeconds * 1000) {
+        break
+      }
+    }
+    ret.push(message)
+    sumTextLength += message.text.length
+    if (sumTextLength >= maxTextLength) {
+      break
+    }
+    if (ret.length >= maxMessageCount) {
+      break
+    }
+  }
+  return ret
 }
 
 function getUserName(message: MessageObjectT): string {
-    if (message.user_id == myUserId) {
-        return myName
-    }
-    if (message.user == null) {
-        return `${message.user_id}`
-    }
-    const { user } = message
-    if (user.display_name && user.display_name.length > 0) {
-        return user.display_name
-    }
-    return user.name
+  if (message.user_id == myUserId) {
+    return myName
+  }
+  if (message.user == null) {
+    return `${message.user_id}`
+  }
+  const { user } = message
+  if (user.display_name && user.display_name.length > 0) {
+    return user.display_name
+  }
+  return user.name
 }
 
-function getPrompt(messages: MessageObjectT[], channel: ChannelObjectT): string {
-    const userNames = new Set([myName])
-    messages.forEach((message) => {
-        if (message.user) {
-            userNames.add(getUserName(message))
-        }
-    })
-
-    let prompt = `Please respond to the conversation given after '--'.
-Use feminine and polite language, and words like 'ですわ', 'ますわ', 'ませんわ', 'ましたわ', 'でしたわ', 'でしょうね' when appropriate.
-The conversation is between ${userNames.size} users, with each line formatted as '[Name]:Statement' and your name is '${myName}'.
-When you refer to yourself in your response, use 私.
-
---
-`
-    // messagesは降順（最新の投稿が[0]に入っているので逆順で処理する
-    for (const message of messages.reverse()) {
-        const userName = getUserName(message)
-        const text = message.text?.replace(/^\n+/, "").replace(/\n+$/, "").replace(/^\s+/, "").replace(/\s+$/, "")
-        prompt += `[${userName}]:${text}\n`
+function getPrompt(messages: MessageObjectT[], channel: ChannelObjectT): any {
+  const userNames = new Set([myName])
+  messages.forEach((message) => {
+    if (message.user) {
+      userNames.add(getUserName(message))
     }
-    prompt += `[${myName}]:`
-    return prompt
+  })
+  let chat = []
+  let instruction = `This is an instruction for you describing how to act.
+Your name is '${myName}'.
+Please respond to the conversation between users and '${myName}' as a friend.
+Do not actively try to help users.
+Do not say, for example, "Is there anything else I can help you with?" or "if you have any other questions?".
+Use feminine and polite language, and words like 'ですわ', 'ますわ', 'ませんわ', 'ましたわ', 'でしたわ', 'でしょうね' when appropriate.
+The conversation is between ${userNames.size} users, with each line formatted as '[Name]:Statement'.
+'Name' is the user's name and 'Statement' is what the user said.
+When you refer to yourself in your response, use 私.
+`
+  chat.push({
+    "role": "system",
+    "content": instruction
+  })
+  // messagesは降順（最新の投稿が[0]に入っているので逆順で処理する
+  for (const message of messages.reverse()) {
+    const userName = getUserName(message)
+    const text = message.text?.replace(/^\n+/, "").replace(/\n+$/, "").replace(/^\s+/, "").replace(/\s+$/, "")
+    if (userName == myName) {
+      chat.push({
+        "role": "assistant",
+        "content": text,
+      })
+    } else {
+      const prompt = `[${userName}]:${text}\n`
+      chat.push({
+        "role": "user",
+        "content": prompt,
+      })
+    }
+  }
+  return chat
 }
 
 function post(methodUrl: string, body: any): Promise<any> {
-    for (const key of Object.keys(body)) {
-        if (body[key] == null) {
-            delete body[key]
-        }
+  for (const key of Object.keys(body)) {
+    if (body[key] == null) {
+      delete body[key]
     }
-    const endpointUrl = `https://beluga.fm/api/v1/${methodUrl}`
-    return new Promise((resolve, reject) => {
-        // @ts-ignore
-        oauth.post(endpointUrl, accessToken, accessTokenSecret, body, function (error, data, res) {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(data)
-            }
-        })
+  }
+  const endpointUrl = `https://beluga.fm/api/v1/${methodUrl}`
+  return new Promise((resolve, reject) => {
+    // @ts-ignore
+    oauth.post(endpointUrl, accessToken, accessTokenSecret, body, function (error, data, res) {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(data)
+      }
     })
+  })
 }
 function get(methodUrl: string, query: any): Promise<any> {
-    for (const key of Object.keys(query)) {
-        if (query[key] == null) {
-            delete query[key]
-        }
+  for (const key of Object.keys(query)) {
+    if (query[key] == null) {
+      delete query[key]
     }
-    const endpointBaseUrl = `https://beluga.fm/api/v1/${methodUrl}`
-    const endpointUrl = new URL(endpointBaseUrl)
-    endpointUrl.search = qs.stringify(query)
-    return new Promise((resolve, reject) => {
-        oauth.get(endpointUrl.toString(), accessToken, accessTokenSecret, function (error, data, res) {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(data)
-            }
-        })
+  }
+  const endpointBaseUrl = `https://beluga.fm/api/v1/${methodUrl}`
+  const endpointUrl = new URL(endpointBaseUrl)
+  endpointUrl.search = qs.stringify(query)
+  return new Promise((resolve, reject) => {
+    oauth.get(endpointUrl.toString(), accessToken, accessTokenSecret, function (error, data, res) {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(data)
+      }
     })
+  })
 }
 
 function getChannelData(channelId: number): ChannelObjectT {
-    return mapChannelIdToChannelObject[channelId]
+  return mapChannelIdToChannelObject[channelId]
 }
 
 async function fetchChannelData(channelId: number) {
-    const response = await get("channel/show", {
-        id: channelId,
-    })
-    const data = JSON.parse(response)
-    if (data.ok == false) {
-        throw new Error("Channel not found")
-    }
-    const { channel } = data
-    mapChannelIdToChannelObject[channelId] = channel
+  const response = await get("channel/show", {
+    id: channelId,
+  })
+  const data = JSON.parse(response)
+  if (data.ok == false) {
+    throw new Error("Channel not found")
+  }
+  const { channel } = data
+  mapChannelIdToChannelObject[channelId] = channel
 }
 
 function sleep(sec: number): Promise<void> {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve()
-        }, sec * 1000)
-    })
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, sec * 1000)
+  })
 }
 
 async function fetchContextMessages(channelId: number): Promise<MessageObjectT[]> {
-    const response = await get("timeline/channel", {
-        channel_id: channelId,
-    })
-    const data = JSON.parse(response)
-    // 中身は降順になっている
-    return getContextMessages(data.messages)
+  const response = await get("timeline/channel", {
+    channel_id: channelId,
+  })
+  const data = JSON.parse(response)
+  // 中身は降順になっている
+  return getContextMessages(data.messages)
 }
 
 function shouldRespondTo(contextMessages: MessageObjectT[]) {
-    if (contextMessages.length == 0) {
-        return false
-    }
-    if (contextMessages[0].user_id == myUserId) {
-        // 自分の投稿には反応しない
-        return false
-    }
-    return true
+  if (contextMessages.length == 0) {
+    return false
+  }
+  if (contextMessages[0].user_id == myUserId) {
+    // 自分の投稿には反応しない
+    return false
+  }
+  return true
 }
 
 async function postResponse(channelId: number) {
-    if (getChannelData(channelId) == null) {
-        await fetchChannelData(channelId)
-    }
-    const contextMessages = await fetchContextMessages(channelId)
-    if (shouldRespondTo(contextMessages) == false) {
-        return
-    }
-    const channel = getChannelData(channelId)
-    const prompt = getPrompt(contextMessages, channel)
-    console.group("Prompt:")
-    console.log(prompt)
+  if (getChannelData(channelId) == null) {
+    await fetchChannelData(channelId)
+  }
+  const contextMessages = await fetchContextMessages(channelId)
+  if (shouldRespondTo(contextMessages) == false) {
+    return
+  }
+  const channel = getChannelData(channelId)
+  const prompt = getPrompt(contextMessages, channel)
+  console.group("Prompt:")
+  console.log(prompt)
+  console.groupEnd()
+  const answer = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: prompt,
+    max_tokens: 512,
+    temperature: 0.5,
+    frequency_penalty: 0.5,
+  })
+  const obj = answer.data.choices[0]
+  if (obj.message) {
+    const text = obj.message.content.replace(new RegExp(`\\[?${myName}\\]?:`, "g"), "")
+    console.group("Chat:")
+    console.log(text)
     console.groupEnd()
-    const answer = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        max_tokens: 256,
-        temperature: 0.5,
-        frequency_penalty: 0.5,
+    await post("message/post", {
+      channel_id: channelId,
+      text: text,
     })
-    const obj = answer.data.choices[0]
-    if (obj.text) {
-        const text = obj.text.replace(/^\n+/, "").replace(/\n+$/, "").replace(/^\s+/, "").replace(/\s+$/, "")
-        console.group("Completion:")
-        console.log(text)
-        console.groupEnd()
-        await post("message/post", {
-            channel_id: channelId,
-            text: text,
-        })
-    }
+  }
 }
 
 async function main() {
-    const ws = new WebSocketClient("wss://beluga.fm/ws/", async (channelId) => {
-        if (!targetChannelIds.includes(channelId)) {
-            return
-        }
-        if (lock[channelId]) {
-            return
-        }
-        lock[channelId] = true
-        let succeeded = false
-        try {
-            await sleep(waitNewMessagesUntil)
-            for (let n = 0; n <= retryLimit; n++) {
-                try {
-                    await postResponse(channelId)
-                    succeeded = true
-                    break
-                } catch (error) {
-                    console.error(error)
-                    succeeded = false
-                    await sleep(10)
-                }
-            }
-        } catch (error) {
-            console.error(error)
-            succeeded = false
-        }
-        if (succeeded == false) {
-            try {
-                await post("message/post", {
-                    channel_id: 4,
-                    text: "エラー",
-                })
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        lock[channelId] = false
-    })
-    ws.connect()
-    try {
-        for (const channelId of targetChannelIds) {
-            await post("message/post", {
-                channel_id: channelId,
-                text: "起動しました",
-            })
-        }
-    } catch (error) {
-        console.error(error)
+  const ws = new WebSocketClient("wss://beluga.fm/ws/", async (channelId) => {
+    if (!targetChannelIds.includes(channelId)) {
+      return
     }
+    if (lock[channelId]) {
+      return
+    }
+    lock[channelId] = true
+    let succeeded = false
+    try {
+      await sleep(waitNewMessagesUntil)
+      for (let n = 0; n <= retryLimit; n++) {
+        try {
+          await postResponse(channelId)
+          succeeded = true
+          break
+        } catch (error) {
+          console.error(error)
+          succeeded = false
+          await sleep(10)
+        }
+      }
+    } catch (error) {
+      console.error(error)
+      succeeded = false
+    }
+    if (succeeded == false) {
+      try {
+        await post("message/post", {
+          channel_id: 4,
+          text: "エラー",
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    lock[channelId] = false
+  })
+  ws.connect()
+  try {
+    for (const channelId of targetChannelIds) {
+      await post("message/post", {
+        channel_id: channelId,
+        text: "起動しました",
+      })
+    }
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 const signals = [
-    "SIGHUP",
-    "SIGINT",
-    "SIGQUIT",
-    "SIGILL",
-    "SIGTRAP",
-    "SIGABRT",
-    "SIGBUS",
-    "SIGFPE",
-    "SIGUSR1",
-    "SIGSEGV",
-    "SIGUSR2",
-    "SIGTERM",
+  "SIGHUP",
+  "SIGINT",
+  "SIGQUIT",
+  "SIGILL",
+  "SIGTRAP",
+  "SIGABRT",
+  "SIGBUS",
+  "SIGFPE",
+  "SIGUSR1",
+  "SIGSEGV",
+  "SIGUSR2",
+  "SIGTERM",
 ]
 signals.forEach(function (sig) {
-    process.on(sig, function () {
-        terminator(sig)
-        console.log("signal: " + sig)
-    })
+  process.on(sig, function () {
+    terminator(sig)
+    console.log("signal: " + sig)
+  })
 })
 
 function terminator(sig: string) {
-    if (typeof sig === "string") {
-        post("message/post", {
-            channel_id: 4,
-            text: "停止しました",
-        }).then(() => {
-            process.exit(1)
-        })
-    }
+  if (typeof sig === "string") {
+    post("message/post", {
+      channel_id: 4,
+      text: "停止しました",
+    }).then(() => {
+      process.exit(1)
+    })
+  }
 }
 
 main()
