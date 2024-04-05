8ee63fb6820ed1d2d97f79d0a2961af8327ddbaf
belugafm <13643825+belugafm@users.noreply.github.com>
2024-04-05 12:57:21 +0900

    Block some users

--- examples/gemini/config.ts
+++ examples/gemini/config.ts
@@ -1,5 +1,5 @@
 export const myUserId = 51721
-export const skipUserIds = [106, 92]
+export const skipUserIds = [106, 92, 51721, 51736]
 export const myName = "gemini"
 export const targetChannelIds = [4]
 export const retryLimit = 3
